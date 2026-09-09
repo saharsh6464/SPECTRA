3e531838416e0217ec6709cebf28ff40709b14e6
Saharsh Patil <saharsh.patil121@gmail.com>
2026-09-09 23:32:39 +0530

    Added GetMcqByID API

--- Frontend/Online-Coding-Platform/src/api/company.js
+++ Frontend/Online-Coding-Platform/src/api/company.js
@@ -1,16 +1,4 @@
-import axiosInstance from "./axiosSetup";
-
-export const getCompanies = async () => {
-  try {
-    const response = await axiosInstance.get('/companies');
-    return response.data;
-  } catch (e) {
-    console.error("Error occurred while fetching companies:", e);
-    throw e;
-  }
-};
-
-export const getCompanyById = async (id) => {
+  export const getCompanyById = async (id) => {
   try {
     const response = await axiosInstance.get(`/companies/${id}`);
     return response.data;
